b86cb13cb6edf12570ccd43e7c369cf5713715aa
aselsmeyer <aselsmeyer@yahoo.com>
2016-06-09 14:40:42 -0500

    Add in comments

--- run_analysis.R
+++ run_analysis.R
@@ -3,11 +3,15 @@ library(data.table)
 
 setInternet2(TRUE)
 
+# Download the data
+
 url <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
 
 f <- file.path(getwd(), "Dataset.zip")
 
 download.file(url, f)
+
+# Unzip the file
 
 temp <- unzip(f)
 
@@ -15,4 +19,12 @@ files <- list.files(getwd())
 
 file <- paste(f)
 
+# Read the dataset and create a data table
+
 dt <- data.table(read.csv(file))
+
+# head(dt, 3)
+# Show all tables in memory and a summary of each
+# tables()
+# Subset data table 
+# dt[3,]
